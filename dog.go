be98135fa3ab12ff46584852814ca2486c2cdddb
komachihashimoto <joujinameko@gmail.com>
2024-01-15 14:20:57 +0900

    比較

--- dog.go
+++ dog.go
@@ -8,6 +8,18 @@ type dog struct {
 	height int
 }
 
+func bigger(d1 dog, d2 dog) string {
+	diff := d1.height - d2.height
+	switch {
+	case diff > 5:
+		return "より大きい"
+	case diff < -5:
+		return "より小さい"
+	default:
+		return "と同じ大きさ"
+	}
+}
+
 func main(){
 	pome := dog{"ポメ","ポメラニアン", 25}
 	maru := dog{"マル","マルチーズ", 22}
@@ -16,6 +28,11 @@ func main(){
 	dogs := []dog{pome, maru, shiba}
 
 	for i := 0; i < len(dogs); i++ {
-		fmt.Printf("%sの%sさんです\n", dogs[i].group, dogs[i].name)
+		next := i + 1
+		if next >= len(dogs) {
+			next = 0
+		}
+		fmt.Printf("%sさんは%sさん%sです\n", dogs[i].name, dogs[next].name,
+				bigger(dogs[i], dogs[next]))
 	}
 }
